5b0302f17e5d2215326137c611700b78885b6f24
ahdabelle <ahdasy21@gmail.com>
2023-06-14 20:56:54 +0200

    Calculation

---
+++ MO53-TD/calculation.h
@@ -0,0 +1,15 @@
+#ifndef CALCULATION_H
+#define CALCULATION_H
+
+class calculation { // pure virtual
+};
+
+class average : public calculation{
+
+};
+
+class rolling_average{
+
+};
+
+#endif // CALCULATION_H
