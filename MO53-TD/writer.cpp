e87757b22d27ca13a82fef7c1b07765984e9a938
ahdabelle <ahdasy21@gmail.com>
2023-06-14 20:57:53 +0200

    writer

---
+++ MO53-TD/writer.cpp
@@ -0,0 +1,7 @@
+#include "writer.h"
+
+// Implement the writer classes (e.g., text_writer, bin_writer)
+writer::writer()
+{
+
+}
---
+++ MO53-TD/writer.h
@@ -0,0 +1,17 @@
+#ifndef WRITER_H
+#define WRITER_H
+
+
+class writer { // pure virtual
+};
+
+
+class text_writer : public writer {
+
+};
+
+class bin_writer : public writer{
+
+};
+
+#endif  // WRITER_H
