759d4b538f74cb6d78741010cbdb4f68ec597f64
ahdabelle <ahdasy21@gmail.com>
2023-06-14 21:37:23 +0200

    Update process.cpp

--- MO53-TD/process.cpp
+++ MO53-TD/process.cpp
@@ -1,6 +1,11 @@
 #include "process.h"
 
-process::process()
-{
+process::process(storage_type* stor, loader* load, calculation* calc, writer* write)
+    : _storage(stor), _loader(load), _calc(calc), _writer(write) {}
 
+void process::execute() {
+	// /!\ Think of checking for nullptr first
+	*_storage = _loader->load(path_to_file);
+	_calc->calculate(_storage);
+	_writer->write(_storage);
 }
