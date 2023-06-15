7f8db2db4f3f1849285577cb052b13299ce20616
ahdabelle <ahdasy21@gmail.com>
2023-06-15 19:35:23 +0200

    Add comments manager

--- MO53-TD/manager.cpp
+++ MO53-TD/manager.cpp
@@ -1,6 +1,5 @@
 #include "manager.h"
 
-manager::manager()
-{
-
+manager::manager() {
+    // Default constructor implementation
 }
--- MO53-TD/manager.h
+++ MO53-TD/manager.h
@@ -6,23 +6,36 @@
 #include "calculation.h"
 #include "writer.h"
 
-class manager
-{
-
+class manager {
 public:
+    /**
+     * @brief Constructs a manager object.
+     */
     manager();
 
-
+    /**
+     * @brief Creates a storage_type object.
+     * @return The created storage_type object.
+     */
     storage_type make_storage();
 
+    /**
+     * @brief Creates a loader object.
+     * @return The created loader object.
+     */
     loader make_reader();
 
+    /**
+     * @brief Creates a calculation object.
+     * @return The created calculation object.
+     */
     calculation make_calculation();
 
+    /**
+     * @brief Creates a writer object.
+     * @return The created writer object.
+     */
     writer make_writer();
-
 };
-
-
 
 #endif // MANAGER_H
