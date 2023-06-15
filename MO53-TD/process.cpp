29554b872e2e300cc6f62efb556fa1cbfb205e64
ahdabelle <ahdasy21@gmail.com>
2023-06-15 19:35:30 +0200

    Add comments process

--- MO53-TD/process.cpp
+++ MO53-TD/process.cpp
@@ -1,11 +1,21 @@
 #include "process.h"
 
 process::process(storage_type* stor, loader* load, calculation* calc, writer* write)
-    : _storage(stor), _loader(load), _calc(calc), _writer(write) {}
+    : _storage(stor), _loader(load), _calc(calc), _writer(write) {
+    // Constructor initializes member variables with provided objects
+}
 
-void process::execute() {
-	// /!\ Think of checking for nullptr first
-	*_storage = _loader->load(path_to_file);
-	_calc->calculate(_storage);
-	_writer->write(_storage);
+void process::execute(const std::string& path_to_file) {
+    // Execute the process steps
+    if (_storage != nullptr && _loader != nullptr && _calc != nullptr && _writer != nullptr) {
+        *_storage = _loader->load(path_to_file);
+        _calc->calculate(_storage);
+        _writer->write(_storage);
+    }
+    // Handle the case where any of the objects are nullptr
+    else {
+        // Handle the error or provide appropriate error handling logic
+        // For example, throw an exception or display an error message
+        throw std::runtime_error("Null pointer encountered in process execution.");
+    }
 }
--- MO53-TD/process.h
+++ MO53-TD/process.h
@@ -7,16 +7,19 @@
 #include "writer.h"
 
 class process {
-    storage_type *_storage;
-    loader *_loader;
-    calculation *_calc;
-    writer *_writer;
+    storage_type *_storage;   // Pointer to storage object
+    loader *_loader;          // Pointer to loader object
+    calculation *_calc;       // Pointer to calculation object
+    writer *_writer;          // Pointer to writer object
 
 public:
-
+    // Constructor
     process(storage_type *stor, loader *load, calculation *calc, writer *write);
 
+    // Execute the process steps
     void execute();
+
+    // Default constructor
     process();
 };
 
