bc8eef212d471f4c80eacbc61e32bb1c26ecd865
ahdabelle <ahdasy21@gmail.com>
2023-06-15 19:37:56 +0200

    Add comments writer

--- MO53-TD/writer.cpp
+++ MO53-TD/writer.cpp
@@ -1,2 +1,9 @@
 #include "writer.h"
 
+// No implementation for the base writer class since it is pure virtual
+
+// Implementation for the text writer
+// ...
+
+// Implementation for the binary writer
+// ...
--- MO53-TD/writer.h
+++ MO53-TD/writer.h
@@ -1,17 +1,16 @@
 #ifndef WRITER_H
 #define WRITER_H
 
-
-class writer { // pure virtual
+class writer {
+    // Pure virtual class for writers
 };
-
 
 class text_writer : public writer {
-
+    // Text writer implementation
 };
 
-class bin_writer : public writer{
-
+class bin_writer : public writer {
+    // Binary writer implementation
 };
 
 #endif  // WRITER_H
