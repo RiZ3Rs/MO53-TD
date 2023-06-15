7c213c19fe55004664c8d8f8333c267c15a28136
ahdabelle <ahdasy21@gmail.com>
2023-06-15 19:35:41 +0200

    Add comments storage_type

--- MO53-TD/storage_type.cpp
+++ MO53-TD/storage_type.cpp
@@ -1,3 +1,14 @@
 #include "storage_type.h"
 
+void storage_type::push_back() {
+    // Implementation for pushing back an element in the base class
+}
+
+void storage_type::pop_element() {
+    // Implementation for popping an element in the base class
+}
+
+void storage_type::iterator() {
+    // Implementation for creating an iterator in the base class
+}
 
--- MO53-TD/storage_type.h
+++ MO53-TD/storage_type.h
@@ -1,26 +1,24 @@
 #ifndef STORAGE_TYPE_H
 #define STORAGE_TYPE_H
 
-class storage_type { 
-
-public :
-
+class storage_type {
+public:
+    // Virtual function for pushing back an element
     virtual void push_back();
 
+    // Virtual function for popping an element
     virtual void pop_element();
 
-    
+    // Virtual function for creating an iterator
     virtual void iterator();
-
 };
 
-
-class list_storage : public storage_type{
-
+class list_storage : public storage_type {
+    // Implementation specific to list storage
 };
 
-class vector_storage : public storage_type{
-
+class vector_storage : public storage_type {
+    // Implementation specific to vector storage
 };
 
 #endif  // STORAGE_TYPE_H
